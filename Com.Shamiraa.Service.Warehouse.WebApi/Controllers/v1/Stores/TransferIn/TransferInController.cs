8596d0906f04f347ab6e3e37d4ac41981c1f599a
Lucia Widyana <mo_cia@yahoo.com>
2022-09-30 10:18:14 +0700

    update code SHM - ALS

--- Com.Shamiraa.Service.Warehouse.WebApi/Controllers/v1/Stores/TransferIn/TransferInController.cs
+++ Com.Shamiraa.Service.Warehouse.WebApi/Controllers/v1/Stores/TransferIn/TransferInController.cs
@@ -18,7 +18,7 @@ namespace Com.Shamiraa.Service.Warehouse.WebApi.Controllers.v1.Stores.TransferIn
 {
     [Produces("application/json")]
     [ApiVersion("1.0")]
-    [Route("v{version:apiVersion}/stores/shm-tb-bbt")]
+    [Route("v{version:apiVersion}/stores/ALS-tb-bbt")]
     [Authorize]
     public class TransferInController : Controller
     {
